0c7e5594f8107283652addc5b6a9548a3e0ca8bb
markligeti <ligetimark.e@gmail.com>
2020-08-17 12:14:04 +0200

    enable eureka annotation

--- src/main/java/com/codecool/battleship/routingandfilteringgateway/RoutingAndFilteringGatewayApplication.java
+++ src/main/java/com/codecool/battleship/routingandfilteringgateway/RoutingAndFilteringGatewayApplication.java
@@ -3,10 +3,12 @@ package com.codecool.battleship.routingandfilteringgateway;
 import com.codecool.battleship.routingandfilteringgateway.filters.pre.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class RoutingAndFilteringGatewayApplication {
 
